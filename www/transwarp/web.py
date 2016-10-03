2311942117f515c53487ee70afdd5419cc212ba7
guti <guwei199087@gmail.com>
2016-10-04 03:44:48 +0800

    网络框架web模块的中Request实现。

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -2,13 +2,24 @@
 __author__ = 'guti'
 
 
+import os
 import threading
 import datetime
 import re
 import urllib
+import string
+import mimetypes
+import cgi
+import logging
 
+from copy import deepcopy
 from db import Dict
 
+
+# letters and digits
+_LETTERS_DIGITS = string.letters + string.digits
+# block size when read the file.
+_BLOCK_SIZE = 1024 * 8
 # thread local context object.
 context = threading.local()
 # define constant for 0 timedelta.
@@ -17,6 +28,8 @@ _TIMEDELTA_ZERO = datetime.timedelta(0)
 _RE_TZ = re.compile(r'^([\+\-])([0-9]{1,2}):([0-9]{1,2})$')
 # a compiled regular expression for response status.
 _RE_RESPONSE_STATUS = re.compile(r'^\d\d\d( [\w ]+)?$')
+# a compiled regular expression for route.
+_RE_ROUTE = re.compile(r'(:[a-zA-Z_]\w*)')
 # all known response status.
 _RESPONSE_STATUSES = {
     # Informational
@@ -153,7 +166,9 @@ class UTC(datetime.tzinfo):
     >>> d2.seconds
     28800
     """
-    def __init__(self, utc):
+
+    def __init__(self, utc, *args, **kwargs):
+        super(UTC, self).__init__(*args, **kwargs)
         utc = str(utc.strip().upper())
         mt = _RE_TZ.match(utc)
         if mt:
@@ -180,6 +195,9 @@ class UTC(datetime.tzinfo):
         return 'UTC time zone information object (%s)' % self._tz_name
 
     __repr__ = __str__
+
+# zero UTC time.
+_UTC_0 = UTC('+00:00')
 
 
 class HttpError(Exception):
@@ -440,14 +458,14 @@ def post(path):
     @get('/:id')
     def index(id):
         pass
-    >>> @get('/test/:id')
+    >>> @post('/test/:id')
     ... def test():
     ...     return 'ok'
     ...
     >>> test.__web_route__
     '/test/:id'
     >>> test.__web_method__
-    'GET'
+    'POST'
     >>> test()
     'ok'
     """
@@ -458,31 +476,444 @@ def post(path):
     return _decorator
 
 
+class Route(object):
+    """
+    Route object for record route information
+
+    >>> @post('/test/:id')
+    ... def test():
+    ...     return 'ok'
+    ...
+    >>> r = Route(test)
+    >>> r.path
+    '/test/:id'
+    >>> r.method
+    'POST'
+    >>> r.is_static
+    False
+    """
+    def __init__(self, func):
+        self.path = func.__web_route__
+        self.method = func.__web_method__
+        self.is_static = _RE_ROUTE.search(self.path) is None
+        if not self.is_static:
+            self.route = re.compile(self._build_regex(self.path))
+        self.func = func
+
+    @staticmethod
+    def _build_regex(path):
+        r"""
+        Convert route path to regular expression.
+
+        >>> @post('/test/:id')
+        ... def test():
+        ...     return 'ok'
+        ...
+        >>> r = Route(test)
+        >>> Route._build_regex('/path/to/:file')
+        '^\\/path\\/to\\/(?P<file>[^\\/]+)$'
+        >>> Route._build_regex('/:user/:comments/list')
+        '^\\/(?P<user>[^\\/]+)\\/(?P<comments>[^\\/]+)\\/list$'
+        >>> Route._build_regex(':id-:pid/:w')
+        '^(?P<id>[^\\/]+)\\-(?P<pid>[^\\/]+)\\/(?P<w>[^\\/]+)$'
+        """
+        re_list = ['^']
+        var_list = list()
+        is_var = False
+        for v in _RE_ROUTE.split(path):
+            if is_var:
+                var_name = v[1:]
+                var_list.append(var_name)
+                re_list.append(r'(?P<%s>[^\/]+)' % var_name)
+            else:
+                s = ''
+                for ch in v:
+                    if ch in _LETTERS_DIGITS:
+                        s += ch
+                    else:
+                        s += '\\' + ch
+                re_list.append(s)
+            is_var = not is_var
+        re_list.append('$')
+        return ''.join(re_list)
+
+    def match(self, url):
+        m = self.route.match(url)
+        return m.groups() if m else None
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __str__(self):
+        status = 'static' if self.is_static else 'dynamic'
+        return 'Route %s: %s, path=%s' % (status, self.method, self.path)
+
+    __repr__ = __str__
+
+
+class StaticFileRoute(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.is_static = False
+        self.route = re.compile('^/static/(.+)$')
+
+    def match(self, url):
+        if url.startwith('/static/'):
+            return url[1:]
+        return None
+
+    def __call__(self, *args):
+        file_path = os.path.join(context.application.document_root, args[0])
+        if not os.path.isdir(file_path):
+            raise not_found()
+        file_ext = os.path.splitext(file_path)[1]
+        context.response.content_type = mimetypes.types_map.get(file_ext, 'application/octet-stream')
+        return self._static_file_generator(file_path)
+
+    @staticmethod
+    def _static_file_generator(file_path):
+        with open(file_path, 'rb') as f:
+            block = f.read(_BLOCK_SIZE)
+            while block:
+                yield block
+                block = f.read(_BLOCK_SIZE)
+
+
+class MultiPartFile(object):
+    """
+    Multi Part file storage get from request input.
+
+    f = ctx.request['file']
+    f.filename # 'test.png'
+    f.file # file-like object
+    """
+    def __init__(self, storage):
+        self.filename = _to_encode(storage.filename)
+        self.file = storage.file
+
+
 class Request(object):
-    # get value from request by key
+    """
+    Request object for obtaining all http request information.
+    """
+    def __init__(self, env):
+        # record the environment of the request object.
+        self._env = env
+
+    def _parse_input(self):
+        def _convert(item):
+            if isinstance(item, list):
+                return [_to_encode(o.value) for o in item]
+            if item.filename:
+                return MultiPartFile(item)
+            return _to_encode(item.value)
+        fs = cgi.FieldStorage(fp=self._env['wsgi.input'], environ=self._env, keep_blank_values=True)
+        inputs = dict()
+        for key in fs:
+            inputs[key] = _convert(fs[key])
+        return inputs
+
+    def _get_raw_input(self):
+        """
+        Get raw input as dict containing values as unicode, list or MultiPartFile.
+        """
+        if not hasattr(self, '_raw_input'):
+            self._raw_input = self._parse_input()
+        return self._raw_input
+
+    def __getitem__(self, item):
+        r"""
+        Get input parameter value. If the specified key has multiple value, the first one is returned.
+        If the specified key is not exist, then raise KeyError.
+
+        >>> from StringIO import StringIO
+        >>> r = Request({'REQUEST_METHOD':'POST', 'wsgi.input':StringIO('a=1&b=M%20M&c=ABC&c=XYZ&e=')})
+        >>> r['a']
+        u'1'
+        >>> r['c']
+        u'ABC'
+        >>> r['empty']
+        Traceback (most recent call last):
+            ...
+        KeyError: 'empty'
+        >>> b = '----WebKitFormBoundaryQQ3J8kPsjFpTmqNz'
+        >>> pl = ['--%s' % b, 'Content-Disposition: form-data; name="name"\n',
+        ... 'Scofield', '--%s' % b, 'Content-Disposition: form-data; name="name"\n',
+        ... 'Lincoln', '--%s' % b, 'Content-Disposition: form-data; name="file"; filename="test.txt"',
+        ... 'Content-Type: text/plain\n', 'just a test', '--%s' % b,
+        ... 'Content-Disposition: form-data; name="id"\n', '4008009001', '--%s--' % b, '']
+        >>> payload = '\n'.join(pl)
+        >>> r = Request({'REQUEST_METHOD':'POST', 'CONTENT_LENGTH':str(len(payload)),
+        ... 'CONTENT_TYPE':'multipart/form-data; boundary=%s' % b, 'wsgi.input':StringIO(payload)})
+        >>> r.get('name')
+        u'Scofield'
+        >>> r.gets('name')
+        [u'Scofield', u'Lincoln']
+        >>> f = r.get('file')
+        >>> f.filename
+        u'test.txt'
+        >>> f.file.read()
+        'just a test'
+        """
+        result = self._get_raw_input()[item]
+        return result[0] if isinstance(result, list) else result
+
     def get(self, key, default=None):
-        pass
+        """
+        Get value from request by key.It's the same as request[key], but return default value if key isn't in request.
 
-    # get key-value dict from input
-    def input(self):
-        pass
+        >>> from StringIO import StringIO
+        >>> r = Request({'REQUEST_METHOD':'POST', 'wsgi.input':StringIO('a=1&b=M%20M&c=ABC&c=XYZ&e=')})
+        >>> r.get('a')
+        u'1'
+        >>> r.get('empty')
+        >>> r.get('empty', 'DEFAULT')
+        'DEFAULT'
+        """
+        result = self._get_raw_input().get(key, default)
+        return result[0] if isinstance(result, list) else result
 
-    # return url information
+    def gets(self, key):
+        """
+        Get multiple values for specified key. If the specified key is not exist, then raise KeyError.
+
+        >>> from StringIO import StringIO
+        >>> r = Request({'REQUEST_METHOD':'POST', 'wsgi.input':StringIO('a=1&b=M%20M&c=ABC&c=XYZ&e=')})
+        >>> r.gets('a')
+        [u'1']
+        >>> r.gets('c')
+        [u'ABC', u'XYZ']
+        >>> r.gets('empty')
+        Traceback (most recent call last):
+            ...
+        KeyError: 'empty'
+        """
+        result = self._get_raw_input()[key]
+        if isinstance(result, list):
+            return deepcopy(result)
+        return [result]
+
+    def input(self, **kwargs):
+        """
+        Get input as dict from request, fill dict using provided default value if key not exist.
+
+        i = ctx.request.input(role='guest')
+        i.role ==> 'guest'
+        >>> from StringIO import StringIO
+        >>> r = Request({'REQUEST_METHOD':'POST', 'wsgi.input':StringIO('a=1&b=M%20M&c=ABC&c=XYZ&e=')})
+        >>> i = r.input(x=2008)
+        >>> i.a
+        u'1'
+        >>> i.b
+        u'M M'
+        >>> i.c
+        u'ABC'
+        >>> i.x
+        2008
+        >>> i.get('d', u'100')
+        u'100'
+        >>> i.x
+        2008
+        """
+        copy = Dict(**kwargs)
+        raw = self._get_raw_input()
+        for k, v in raw.iteritems():
+            copy[k] = v[0] if isinstance(v, list) else v
+        return copy
+
+    def get_body(self):
+        """
+        Get raw data from HTTP POST and return as string.
+
+        >>> from StringIO import StringIO
+        >>> r = Request({'REQUEST_METHOD':'POST', 'wsgi.input':StringIO('<xml><raw/>')})
+        >>> r.get_body()
+        '<xml><raw/>'
+        """
+        fp = self._env['wsgi.input']
+        return fp.read()
+
+    @property
+    def remote_addr(self):
+        """
+        Get remote addr. Return '0.0.0.0' if cannot get remote_addr.
+
+        >>> r = Request({'REMOTE_ADDR': '192.168.0.100'})
+        >>> r.remote_addr
+        '192.168.0.100'
+        """
+        return self._env.get('REMOTE_ADDR', '0.0.0.0')
+
+    @property
+    def document_root(self):
+        """
+        Get raw document_root as str. Return '' if no document_root.
+        >>> r = Request({'DOCUMENT_ROOT': '/srv/path/to/doc'})
+        >>> r.document_root
+        '/srv/path/to/doc'
+        """
+        return self._env.get('DOCUMENT_ROOT', '')
+
+    @property
+    def query_string(self):
+        """
+        Get raw query string as str. Return '' if no query string.
+        >>> r = Request({'QUERY_STRING': 'a=1&c=2'})
+        >>> r.query_string
+        'a=1&c=2'
+        >>> r = Request({})
+        >>> r.query_string
+        ''
+        """
+        return self._env.get('QUERY_STRING', '')
+
+    @property
+    def environment(self):
+        """
+        Get raw environ as dict, both key, value are str.
+
+        >>> r = Request({'REQUEST_METHOD': 'GET', 'wsgi.url_scheme':'http'})
+        >>> r.environment.get('REQUEST_METHOD')
+        'GET'
+        >>> r.environment.get('wsgi.url_scheme')
+        'http'
+        >>> r.environment.get('SERVER_NAME')
+        >>> r.environment.get('SERVER_NAME', 'name')
+        'name'
+        """
+        return self._env
+
+    @property
+    def request_method(self):
+        """
+        Get request method. The valid returned values are 'GET', 'POST', 'HEAD'.
+
+        >>> r = Request({'REQUEST_METHOD': 'GET'})
+        >>> r.request_method
+        'GET'
+        >>> r = Request({'REQUEST_METHOD': 'POST'})
+        >>> r.request_method
+        'POST'
+        """
+        return self._env['REQUEST_METHOD']
+
+    # TODO: check _unquote function
     @property
     def path_info(self):
-        pass
+        """
+        Get request path as str.
 
-    # return http headers dict
+        >>> r = Request({'PATH_INFO': '/test/a%20b.html'})
+        >>> r.path_info
+        '/test/a b.html'
+        """
+        return urllib.unquote(self._env.get('PATH_INFO', ''))
+
+    @property
+    def host(self):
+        """
+        Get request host as str. Default to '' if cannot get host.
+
+        >>> r = Request({'HTTP_HOST': 'localhost:8080'})
+        >>> r.host
+        'localhost:8080'
+        """
+        return self._env.get('HTTP_HOST', '')
+
+    def _get_headers(self):
+        if not hasattr(self, '_headers'):
+            headers = dict()
+            for k, v in self._env.iteritems():
+                if k.startswith('HTTP_'):
+                    # convert 'HTTP_ACCEPT_ENCODING' to 'ACCEPT-ENCODING'
+                    headers[k[5:].replace('_', '-').upper()] = _to_encode(v)
+            self._headers = headers
+        return self._headers
+
     @property
     def headers(self):
-        pass
+        """
+        Get all HTTP headers with key as str and value as unicode. The header names are 'XXX-XXX' uppercase.
+
+        >>> r = Request({'HTTP_USER_AGENT': 'Mozilla/5.0', 'HTTP_ACCEPT': 'text/html'})
+        >>> H = r.headers
+        >>> H['ACCEPT']
+        u'text/html'
+        >>> H['USER-AGENT']
+        u'Mozilla/5.0'
+        >>> L = H.items()
+        >>> L.sort()
+        >>> L
+        [('ACCEPT', u'text/html'), ('USER-AGENT', u'Mozilla/5.0')]
+        """
+        return Dict(**self._get_headers())
+
+    def header(self, key, default=None):
+        """
+        Get header from request as unicode, return None if not exist, or default if specified.
+        The header name is case-insensitive such as "USER-AGENT" or u"Content-Type".
+
+        >>> r = Request({'HTTP_USER_AGENT': 'Mozilla/5.0', 'HTTP_ACCEPT': 'text/html'})
+        >>> r.header('User-Agent')
+        u'Mozilla/5.0'
+        >>> r.header('USER-AGENT')
+        u'Mozilla/5.0'
+        >>> r.header('Accept')
+        u'text/html'
+        >>> r.header('Test')
+        >>> r.header('Test', u'DEFAULT')
+        u'DEFAULT'
+        """
+        return self._get_headers().get(key.upper(), default)
+
+    def _get_cookies(self):
+        if not hasattr(self, '_cookies'):
+            cookies = dict()
+            cookie_str = self._env.get('HTTP_COOKIE')
+            if cookie_str:
+                for c in cookie_str.split(';'):
+                    position = c.find('=')
+                    if position > 0:
+                        cookies[c[:position].strip()] = _unquote(c[position+1:])
+            self._cookies = cookies
+        return self._cookies
+
+    @property
+    def cookies(self):
+        """
+        Return all cookies as dict. The cookie name is str and values is unicode.
+        >>> r = Request({'HTTP_COOKIE':'A=123; url=http%3A%2F%2Fwww.example.com%2F'})
+        >>> r.cookies['A']
+        u'123'
+        >>> r.cookies.url
+        u'http://www.example.com/'
+        >>>
+        """
+        return Dict(**self._get_cookies())
 
     # get value by key in cookie
-    def cookie(self, value, default=None):
-        pass
+    def cookie(self, key, default=None):
+        """
+        Return specified cookie value as unicode. Default to None if cookie not exists.
+
+        >>> r = Request({'HTTP_COOKIE':'A=123; url=http%3A%2F%2Fwww.example.com%2F'})
+        >>> r.cookie('A')
+        u'123'
+        >>> r.cookie('url')
+        u'http://www.example.com/'
+        >>> r.cookie('test')
+        >>> r.cookie('test', u'DEFAULT')
+        u'DEFAULT'
+        """
+        return self._get_cookies().get(key, default)
 
 
 class Response(object):
+
+    def __init__(self):
+        self._status = '200 OK'
+        self._headers = {'CONTENT-TYPE': 'text/html; charset=utf-8'}
+
     def set_header(self, key, value):
         pass
 
@@ -559,5 +990,6 @@ class WSGIApplication(object):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     import doctest
     doctest.testmod()
